bc6b987a4cb8da72c726fada182e15936e49dad9
Bobby Rockers <103instructor@103LAB06WKS07.Campus.ittesi.com>
2015-06-29 20:18:13 -0500

    Working print list... that is all

--- application.js
+++ application.js
@@ -1,1 +1,10 @@
-alert("Hello world")
+function addToDo(){
+	var toDoList = [];
+	var list;
+	list = document.getElementById("todolist");
+	
+	toDoList[0] = "Take out the dog"
+	
+	list.innerHTML += '<div class="item">' + toDoList[0] + '</div>';
+	
+}
