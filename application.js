8cbf1d2d6005cd3fe968f8cf343fdee676a57e62
Bobby Rockers <103instructor@103LAB06WKS07.Campus.ittesi.com>
2015-06-29 21:38:54 -0500

    Move some functions around

--- application.js
+++ application.js
@@ -1,10 +1,24 @@
+// Define Global
+var toDoList = [];
+
+function changeDisplay(){
+	var list = document.getElementById("todolist");
+	// Only place we actually change the screen
+	list.innerHTML = "";
+	for(var i=0; i<toDoList.length; i++){
+		list.innerHTML += '<div class="item">' + toDoList[i] + '</div>';
+	}
+}
+
 function addToDo(){
-	var toDoList = [];
-	var list;
-	list = document.getElementById("todolist");
+	var tmpItem;
+	var newToDo = document.getElementById("todonew");	
+	tmpItem = newToDo.value;
+
+	toDoList.push(tmpItem);
+	// Call our display function
+	changeDisplay();
 	
-	toDoList[0] = "Take out the dog"
-	
-	list.innerHTML += '<div class="item">' + toDoList[0] + '</div>';
-	
+	newToDo.value = "";
+	newToDo.select();
 }
